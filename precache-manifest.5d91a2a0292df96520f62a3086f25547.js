0de89a6652314b8a012894ca8fb8efdea3c79341
Shreyas H C <shreyashc123@gmail.com>
2020-10-31 01:02:19 +0530

    Add files via upload

---
+++ precache-manifest.5d91a2a0292df96520f62a3086f25547.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90f7dea85621bfeed80bda61d0cf7941",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f165bd4c98a70682725",
+    "url": "/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "a0a081539bed7e785aa7",
+    "url": "/static/css/main.7f933dd0.chunk.css"
+  },
+  {
+    "revision": "3f165bd4c98a70682725",
+    "url": "/static/js/2.3025fc49.chunk.js"
+  },
+  {
+    "revision": "8e5ae93e18f66c1d607de05f5b5d011b",
+    "url": "/static/js/2.3025fc49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0a081539bed7e785aa7",
+    "url": "/static/js/main.8558fcce.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
